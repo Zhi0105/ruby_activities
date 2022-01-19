d63fe3167884ef199d0bddc8a96091f3e465a04c
Argie Barcena <barcenaargie@gmail.com>
2022-01-19 11:42:21 +0800

    Final commit for first assignment

--- L1_data_types/age.rb
+++ L1_data_types/age.rb
@@ -1,11 +1,11 @@
 print "How old are you as of now? "
-age = gets.chomp
+age = Integer(gets.chomp)
 
 puts "In 10 years you will be:"
-puts age.to_i + 10
+puts age + 10
 puts "In 20 years you will be:"
-puts age.to_i + 20
+puts age + 20
 puts "In 30 years you will be:"
-puts age.to_i + 30
+puts age + 30
 puts "In 40 years you will be:"
-puts age.to_i + 40
+puts age + 40
